be5dee8ffbbb17b8e735d79bb3d1529baaa7d634
power080900 <power080900@gmail.com>
2022-02-24 16:17:31 +0900

    022415L

--- project/project/todaysPlay/views.py
+++ project/project/todaysPlay/views.py
@@ -6,15 +6,15 @@ import random
 def main(request):
     randint = random.randrange(1,6)
     if randint == 1:
-        randomplay = '맛있는거 먹'
+        randomplay = '맛있는거 먹자'
     elif randint == 2:
-        randomplay = '재미있게 놀'
+        randomplay = '재미있게 놀자'
     elif randint == 3:
-        randomplay = '힐링 하'
+        randomplay = '힐링 하자'
     elif randint == 4:
-        randomplay = '운동 하'
+        randomplay = '운동 하자'
     elif randint == 5:
-        randomplay = '구경 가'
+        randomplay = '구경 가자'
     context = {
         'random': randomplay,
     }
