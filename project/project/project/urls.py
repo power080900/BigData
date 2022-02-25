15e457392809a552731be775b3e607c8af360231
power080900 <power080900@gmail.com>
2022-02-25 13:56:14 +0900

    022514L

--- project/project/project/urls.py
+++ project/project/project/urls.py
@@ -21,5 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todaysPlay/', include('todaysPlay.urls')),
-    # path('accounts/', include('accounts.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- project/project/todaysPlay/views.py
+++ project/project/todaysPlay/views.py
@@ -6,9 +6,6 @@ import random
 
 # Create your views here.
 def main(request):
-    # context = None
-    # print(request.user.is_authenticated)
-    # print(request.user)
     randint = random.randrange(1,6)
     if randint == 1:
         randomplay = '맛있는거 먹자'
@@ -20,58 +17,7 @@ def main(request):
         randomplay = '운동 하자'
     elif randint == 5:
         randomplay = '구경 가자'
-    # if request.user.is_authenticated:
     context = {
-            # 'logineduser': request.user,
             'random': randomplay,
         }
     return render(request, 'main.html', context)
-
-def register(request):
-    res_data = None
-    if request.method =='POST':
-        useremail = request.POST.get('useremail')
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        password = request.POST.get('password')
-        re_password = request.POST.get('re-password')
-        res_data = {}
-        if User.objects.filter(username=useremail):
-            res_data['error']='이미 가입된 아이디(이메일주소)입니다.'
-        elif password != re_password:
-            res_data['error']='비밀번호가 다릅니다.'
-        else:
-            user = User.objects.create_user(username = useremail,
-                            first_name = firstname,
-                            last_name = lastname,
-                            password = password)
-            auth.login(request, user)
-            redirect("main.html")
-    return render(request, 'register.html', res_data)  # 교육생들 수정!!!!
-
-
-def login(request):
-    if request.method == "POST":
-        useremail = request.POST.get('useremail', None)
-        password = request.POST.get('password', None)
-        user = auth.authenticate(username=useremail, password=password)
-        #print("***", user.date_joined)
-        if user is not None :
-            auth.login(request, user)
-            return redirect("main:main")
-        else :
-            return render(request, 'login.html', {'error': '사용자 아이디 또는 패스워드가 틀립니다.'})
-    else :
-        return render(request, 'login.html')
-
-def logout(request):
-    if request.user.is_authenticated:
-        auth.logout(request)
-    return redirect("main:main")
-
-def only_member(request) :
-    context = None
-    if request.user.is_authenticated:
-        context = {'logineduser': request.user.last_name+request.user.first_name}
-    return render(request, 'member.html', context)
-
