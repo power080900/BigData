e7c99e49cf0889ae7c8075388b74b1e3d9bfd502
power080900 <power080900@gmail.com>
2022-02-25 13:09:03 +0900

    022511L

--- project/project/todaysPlay/urls.py
+++ project/project/todaysPlay/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+
+app_name = "main"
 urlpatterns = [
-    path('todaysPlay',views.main),
+    # path('todaysPlay',views.main),
+    # path('', views.main, name='main'),
+    # path('register/', views.register, name="register"),
+    # path('login/', views.login, name="login"),
+    # path('logout/', views.logout, name="logout"),
+    # path('onlymember/', views.only_member, name="onlymember"),
 ]
