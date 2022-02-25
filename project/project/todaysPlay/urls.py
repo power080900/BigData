b5320c1f9b4b4329522dac1234c0fd400a776827
power080900 <power080900@gmail.com>
2022-02-25 13:18:43 +0900

    022513L

--- project/project/todaysPlay/urls.py
+++ project/project/todaysPlay/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = "main"
 urlpatterns = [
-    # path('todaysPlay',views.main),
-    # path('', views.main, name='main'),
-    # path('register/', views.register, name="register"),
-    # path('login/', views.login, name="login"),
-    # path('logout/', views.logout, name="logout"),
-    # path('onlymember/', views.only_member, name="onlymember"),
+    path('todaysPlay',views.main),
+    path('', views.main, name='main'),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('onlymember/', views.only_member, name="onlymember"),
 ]
