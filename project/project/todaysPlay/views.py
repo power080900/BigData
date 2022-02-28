ae632ba47b391ef4142688f1ddf936425606019f
power080900 <power080900@gmail.com>
2022-02-28 15:39:58 +0900

    022815L

--- project/project/todaysPlay/views.py
+++ project/project/todaysPlay/views.py
@@ -6,17 +6,35 @@ import random
 
 # Create your views here.
 def main(request):
-    randint = random.randrange(1,6)
+    randint = random.randrange(1,15)
     if randint == 1:
-        randomplay = '맛있는거 먹자'
+        randomplay = '1호선'
     elif randint == 2:
-        randomplay = '재미있게 놀자'
+        randomplay = '2호선'
     elif randint == 3:
-        randomplay = '힐링 하자'
+        randomplay = '3호선'
     elif randint == 4:
-        randomplay = '운동 하자'
+        randomplay = '4호선'
     elif randint == 5:
-        randomplay = '구경 가자'
+        randomplay = '5호선'
+    elif randint == 6:
+        randomplay = '6호선'
+    elif randint == 7:
+        randomplay = '7호선'
+    elif randint == 8:
+        randomplay = '8호선'
+    elif randint == 9:
+        randomplay = '9호선'
+    elif randint == 10:
+        randomplay = '분당선'
+    elif randint == 11:
+        randomplay = '신분당선'
+    elif randint == 12:
+        randomplay = '중앙선'
+    elif randint == 13:
+        randomplay = '경춘선'
+    elif randint == 14:
+        randomplay = '경의선'
     context = {
             'random': randomplay,
         }
